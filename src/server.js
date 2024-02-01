14ebffefca46f4a68484c9dc6f1e4516d74a27e4
GutoVieoli <augustovieiraoli@gmail.com>
2024-02-01 17:41:32 -0300

    Upando pra web

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
 
 const porta = process.env.PORT || 3000;
 const keyWeather = process.env.KEY_WEATHER;
